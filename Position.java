b31951f2bad5861a5e6c85802f26e499960ee9ec
niko111 <niko111@bluewin.ch>
2022-10-31 00:47:41 +0100

    exception invalid input+static method getDistance

--- Position.java
+++ Position.java
@@ -5,22 +5,21 @@ public class Position {
         char ch2Code = userInput.charAt(1);
         String ch2Str = String.valueOf(ch2Code);
         int ch2 = Integer.parseInt(ch2Str);
-        if (ch2 > 9) {
-            //TODO: handle invalid input -> throw exception
-        }
+        
         aCoordinates[1] = ch2;
         char ch1 = Character.toUpperCase(userInput.charAt(0));
         Character[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
         
         int found = -1;
         for (int i = 0; i < letters.length; ++i) {
-        if (letters[i] == ch1) {
-        found = i;
-        break;
+            if (letters[i] == ch1) {
+            found = i;
+            break;
             }
         }
         if (found == -1 ) {
             //TODO: handle invalid input -> throw exception
+            throw new IndexOutOfBoundsException();
         }
         else {
             aCoordinates[0] = found;
@@ -51,5 +50,16 @@ public class Position {
     //method for test purposes...
     public void printPosition() {
         System.out.println("[" + aCoordinates[0] + "," + aCoordinates[1] + "]");
+    }
+
+    static int getDistance(Position position1, Position position2) {
+        int temp;
+        if (position1.getY() == position2.getY()) {
+            temp = position1.getX() - position2.getX();
+        }        
+        else {
+            temp = position1.getY() - position2.getY();
+         }
+        return java.lang.Math.abs(temp);
     }
 }
