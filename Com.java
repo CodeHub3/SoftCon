86a1a6bdbcf05288c66530dfb179ac7237d09236
lorezh <116369831+lorezh@users.noreply.github.com>
2022-10-31 15:14:47 +0100

    Com updated

--- Com.java
+++ Com.java
@@ -6,34 +6,36 @@ public class Com {
     //TODO com
     private ArrayList<Position> aComCall = new ArrayList<>();
 
-    public void comBomb(Position target, Fleet pFleet){
+    //public getComCall (Position target,) getter method for postion to check if call was alrdy made
+
+    public void comBomb(Position target, Fleet pFleet, OceanGrid ocean){
         aComCall.add(target);
         for(Ship ship : pFleet.aFleet){
             for(Position pos : ship.getPositions()){
                 if(pos == target){
-                    OceanGrid.addHit(pos);
+                    ocean.addHit(pos);
                 }
                 else{
-                    OceanGrid.addMiss(pos);
+                    ocean.addMiss(pos);
                 }
             }
         }
     }
 
-    public void playerBombing(Position aim, Fleet cFleet){
+    public void playerBombing(Position aim, Fleet cFleet, TargetGrid tarGrid){
         for(Ship ship : cFleet.aFleet){
             for(Position pos : ship.getPositions()){
                 if(pos == aim){
                     if(ship.getLifespan()==1){
-                        TargetGrid.addHit(ship.getPositions(),ship.getType());
+                        tarGrid.addHit(ship.getPositions(),ship.getType());
                     }
                     else{
-                        TargetGrid.addHit(aim);
+                        tarGrid.addHit(aim);
                     }
                     ship.getLifespan -=1;
                 }
                 else{
-                    TargetGrid.addMiss(aim);
+                    tarGrid.addMiss(aim);
                 }
             }
         }
